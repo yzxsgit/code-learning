bd05372b009d63b3de7225c6fdcb0828ad35580c
yzx <2509371054@qq.com>
2026-01-11 14:31:20 +0800

    finish 单源最短路的扩展应用

---
+++ 算法提高课/图论/单源最短路的扩展应用/1134. 最短路计数.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+const int N = 1e5 + 10, M = 4e5 + 10, mod = 100003;
+
+int n, m;
+int h[N], e[M], ne[M], idx;
+
+void add(int a, int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+int dist[N], cnt[N];
+
+void bfs()
+{
+    memset(dist, 0x3f, sizeof dist);
+    queue<int> q;
+    dist[1] = 0, cnt[1] = 1;
+    q.push(1);
+
+    while (q.size())
+    {
+        int t = q.front();
+        q.pop();
+
+        for (int i = h[t]; ~i; i = ne[i])
+        {
+            int j = e[i];
+            if(dist[j] > dist[t] + 1)
+            {
+                dist[j] = dist[t] + 1;
+                cnt[j] = cnt[t];
+                q.push(j);
+            }
+            else if(dist[j] == dist[t] + 1)
+            {
+                cnt[j] = (cnt[j] + cnt[t]) % mod;
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    memset(h, -1, sizeof h);
+
+    while (m--)
+    {
+        int a, b;
+        cin >> a >> b;
+        add(a, b);
+        add(b, a);
+    }
+
+    bfs();
+
+    for (int i = 1; i <= n; i++)
+        cout << cnt[i] << "\n";
+
+    return 0;
+}
