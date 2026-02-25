25bd87de5d1c666bf22edf950d5cdfd00fc33ccb
home_computer <2509371054@qq.com>
2026-02-26 00:39:37 +0800

    Educational Codeforces Round 187 (Rated for Div. 2)

---
+++ codeforce/Educational Codeforces Round 187 (Rated for Div. 2)/A_Towers_of_Boxes.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <set>
+
+using namespace std;
+using i64 = long long;
+using PII = pair<int,int>;
+
+void solve()
+{
+    int n, m, d;
+    cin >> n >> m >> d;
+
+    int h = d / m;
+    int res = n / (h + 1) + (n % (h + 1) != 0);
+
+    cout << res << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
