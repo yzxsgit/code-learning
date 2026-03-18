6c94765f5f5ae94d50d84c401dd4ee229c93d8e2
ubuntu <2509371054@qq.com>
2026-03-18 19:52:37 +0800

    树

---
+++ 模板题/基础数据结构/树基础/P3621.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <set>
+
+using namespace std;
+using i64 = long long;
+using PII = pair<int,int>;
+
+vector<PII> g;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    g.resize(n + 1);
+    
+    
+
+    return 0;
+}
